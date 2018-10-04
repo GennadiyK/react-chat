187225367f009bd800670e45d2cc49144f544ed5
HenadziK <hkalinouski@exadel.com>
2018-10-04 19:06:17 +0300

    Added icons and main grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,101 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import SimpleBottomNavigation from './BottomNavigation'
 
-class App extends Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appFrame: {
+    height: '100vh',
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    width: `calc(100% - 240px)`,
+  },
+  'appBar-left': {
+    marginLeft: '240px',
+  },
+  'appBar-right': {
+    marginRight: '240px',
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: '240px',
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class PermanentDrawer extends React.Component {
+  state = {
+    anchor: 'left',
+  };
+
+  handleChange = event => {
+    this.setState({
+      anchor: event.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { anchor } = this.state;
+
     return (
-      <h1>Chat</h1>
+      <div className={classes.root}>
+        <div className={classes.appFrame}>
+          <AppBar
+            position="absolute"
+            className={classNames(classes.appBar, classes[`appBar-${anchor}`])}
+          >
+            <Toolbar>
+              <Typography variant="title" color="inherit" noWrap>
+                React chat
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            anchor={anchor}
+          >
+            <div className={classes.toolbar} />
+
+            <List>1</List>
+
+            <List>2</List>
+            <SimpleBottomNavigation/>
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Typography>{'You think water moves fast? You should see ice.'}</Typography>
+          </main>
+        </div>
+      </div>
     );
   }
 }
 
-export default App;
+PermanentDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PermanentDrawer);
---
+++ src/BottomNavigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import ExploreIcon from '@material-ui/icons/Explore';
+
+const styles = {
+
+};
+
+class SimpleBottomNavigation extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation
+        value={value}
+        onChange={this.handleChange}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction label="My chats" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
+      </BottomNavigation>
+    );
+  }
+}
+
+SimpleBottomNavigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleBottomNavigation);
