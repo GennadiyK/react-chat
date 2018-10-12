a0cd19f01f9da0e6b16060a6c600aaf9be5434f8
HenadziK <hkalinouski@exadel.com>
2018-10-12 19:01:49 +0300

    added colorFrom

---
+++ src/utils/color-from.js
@@ -0,0 +1,21 @@
+import red from '@material-ui/core/colors/red';
+import pink from '@material-ui/core/colors/pink';
+import purple from '@material-ui/core/colors/purple';
+import indigo from '@material-ui/core/colors/indigo';
+import blue from '@material-ui/core/colors/blue';
+import teal from '@material-ui/core/colors/teal';
+import green from '@material-ui/core/colors/green';
+
+const colors = [
+  red,
+  pink,
+  purple,
+  indigo,
+  blue,
+  teal,
+  green,
+];
+
+export default function colorFrom(string) {
+
+}
