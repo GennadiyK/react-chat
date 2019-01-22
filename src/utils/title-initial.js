fef8f8cb07b5893f01ec1286806f1add4e8f09ca
hkalinouski <hkalinouski@exadel.com>
2019-01-22 07:45:04 +0300

    removed console log

--- src/utils/title-initial.js
+++ src/utils/title-initial.js
@@ -2,10 +2,7 @@ export default function titleInitials(title) {
   try {
     return title
       .split(' ')
-      .map((word) => {
-        console.log('!!!!', word);
-        return word[0].toUpperCase();
-      })
+      .map(word => word[0].toUpperCase())
       .slice(0, 2)
       .join('');
   } catch (e) {
