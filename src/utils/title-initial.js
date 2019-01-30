99cda4d61f4a9c399b990eeb47f565a9950d70b7
hkalinouski <hkalinouski@exadel.com>
2019-01-30 07:38:34 +0300

    fixed titleInitials

--- src/utils/title-initial.js
+++ src/utils/title-initial.js
@@ -1,8 +1,14 @@
 export default function titleInitials(title) {
   try {
     return title
+      .trim()
       .split(' ')
-      .map(word => word[0].toUpperCase())
+      .map((word) => {
+        if (word.length === 0) {
+          return '...';
+        }
+        return word[0].toUpperCase();
+      })
       .slice(0, 2)
       .join('');
   } catch (e) {
